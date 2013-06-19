8eb4649319235023d26b5bdca1b3b33d00cb8a70
Piedone <piedone@pyrocenter.hu>
2013-06-20 00:38:29 +0200

    Adding JavaScriptAntispamPart

---
+++ Migrations.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Lombiq.Antispam.Models;
+using Orchard.Data.Migration;
+using Orchard.ContentManagement.MetaData;
+using Orchard.Core.Contents.Extensions;
+
+namespace Lombiq.Antispam
+{
+    public class Migrations : DataMigrationImpl
+    {
+        public int Create()
+        {
+            ContentDefinitionManager.AlterPartDefinition(typeof(JavaScriptAntispamPart).Name,
+                builder => builder
+                    .Attachable()
+                    .WithDescription("Prevents spambots to post the editor by requiring JavaScript support."));
+
+            return 1;
+        }
+    }
+}
---
+++ Models/JavaScriptAntispamPart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Orchard.ContentManagement;
+
+namespace Lombiq.Antispam.Models
+{
+    public class JavaScriptAntispamPart : ContentPart
+    {
+        public bool IAmHuman { get; set; }
+    }
+}
